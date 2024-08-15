66e075eaa735058efe228fc2588ba173d5e04b2e
Camilo0291 <acquintanam@gmail.com>
2024-08-15 10:17:55 -0500

    First

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+const Body = () => {
+    return (
+        <main>
+            <h1>Choose a Plan to Grow</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Odio, recusandae
+                fugiat eius eligendi consequatur
+                nesciunt unde beatae suscipit
+                architecto alias. Quia saepe cum
+                delectus nobis necessitatibus quasi,
+                accusantium sit odit?</p>
+        </main>
+    )
+}
+export default Body
---
+++ src/Footer.jsx
@@ -0,0 +1,63 @@
+function Footer() {
+    return (
+        <footer>
+            <section>
+                <button>Mensual</button>
+                <button>Anual</button>
+                <span>20%</span>
+            </section>
+            <div>
+                <section>
+                    <div>
+                        <img src="" alt="" />
+                        <h2>Basic</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+                            eveniet ipsa! Mollitia quis nobis alias asperiores quaerat quidem
+                            quod odio veniam, perspiciatis rem corporis, commodi adipisci,
+                            sed temporibus ut praesentium.
+                        </p>
+                        <div>
+                            <span>$59</span>
+                            <small>per month</small>
+                            <button>Get Started</button>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <img src="" alt="" />
+                        <h2>Basic</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+                            eveniet ipsa! Mollitia quis nobis alias asperiores quaerat quidem
+                            quod odio veniam, perspiciatis rem corporis, commodi adipisci,
+                            sed temporibus ut praesentium.
+                        </p>
+                        <div>
+                            <span>$59</span>
+                            <small>per month</small>
+                            <button>Get Started</button>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <img src="" alt="" />
+                        <h2>Basic</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+                            eveniet ipsa! Mollitia quis nobis alias asperiores quaerat quidem
+                            quod odio veniam, perspiciatis rem corporis, commodi adipisci,
+                            sed temporibus ut praesentium.
+                        </p>
+                        <div>
+                            <span>$59</span>
+                            <small>per month</small>
+                            <button>Get Started</button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
